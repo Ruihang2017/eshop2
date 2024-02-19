54c9025b3d17f2bc70b2d7bd4fbe940060c6da55
Horace HOU <HoraceHou@CoatesGroup.com>
2024-02-19 23:04:36 +1100

    udpated firebase

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -10,28 +10,7 @@ function Subtotal() {
     const [{ basket }, dispatch] = useStateValue();
 
     return (
-        // <div className="subtotal">
-        //     <CurrencyFormat
-        //         renderText={(value) => (
-        //             <>
-        //                 <p>
-        //                     Subtotal ({basket.length} items): <strong>${value}</strong>
-        //                 </p>
-        //                 <small className="subtotal__gift">
-        //                     <input type="checkbox" /> This order contains a gift
-        //                 </small>
-        //             </>
-        //         )}
 
-        //         decimalScale={2}
-        //         value={getBasketTotal(basket)}
-        //         displayType={"text"}
-        //         thousandSeparator={true}
-
-        //     />
-
-        //     <button>Proceed to Checkout</button>
-        // </div>
         <div className="subtotal">
             <CurrencyFormat
                 renderText={(value) => (
---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+// import firebase from "firebase";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCwJM-faKBpbNz6bGc45SDvI7YO7PyWTlE",
+    authDomain: "eshop2-b8ac9.firebaseapp.com",
+    projectId: "eshop2-b8ac9",
+    storageBucket: "eshop2-b8ac9.appspot.com",
+    messagingSenderId: "1025179235605",
+    appId: "1:1025179235605:web:d01728d299b27f064e251c",
+    measurementId: "G-2JD9DJ2DM1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const db = app.firestore();
+const auth = app.auth();
+
+export { db, auth, analytics };
+
